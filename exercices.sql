2b0c2e80c413c5d5293834784f4164e92aceba59
KbabFR <43203741+KbabFR@users.noreply.github.com>
2022-06-22 12:27:30 +0200

    maj exercices

--- exercices.sql
+++ exercices.sql
@@ -129,3 +129,102 @@ GROUP BY (CASE
 		WHEN TIMESTAMPDIFF(YEAR,date_naissance,CURRENT_DATE)>=60 AND TIMESTAMPDIFF(YEAR,date_naissance,CURRENT_DATE)<70 THEN 'tranche 60-70ans'
 		ELSE 'other' 
 	END);
+	
+/* exo 23 Lister tous les livres avec leur auteur */
+SELECT livre.titre, auteur.nom
+FROM livre
+JOIN auteur ON livre.id_auteur = auteur.id;
+
+/* exo 24 Lister tous les livres avec leur auteur et leur editeur */
+SELECT livre.titre, auteur.nom, editeur.nom
+FROM livre
+JOIN auteur ON livre.id_auteur = auteur.id
+JOIN editeur ON livre.id_editeur = editeur.id;
+
+/* exo 25 Afficher la liste des éditeurs avec le nombre de livre de chacun */
+SELECT editeur.nom, COUNT(*) AS nb_livre
+FROM livre
+JOIN editeur ON livre.id_editeur = editeur.id
+GROUP BY livre.id_editeur
+ORDER BY 2 DESC;
+
+/* exo 26 Lister tous les livres actuellement empruntés avec le nom de l'abonné l'ayant emprunté */
+SELECT livre.titre, CONCAT(UPPER(abonne.nom), " ", abonne.prenom) AS NOM_Prenom
+FROM emprunt
+JOIN livre ON emprunt.id_livre = livre.id
+JOIN abonne ON emprunt.id_abonne = abonne.id
+WHERE emprunt.date_retour IS NULL
+ORDER BY 1;
+
+/* exo 27 Trouver les abonnés ayant emprunté un livre depuis plus de 2 mois et ne l’ayant pas rendu */
+SELECT livre.titre, CONCAT(UPPER(abonne.nom), " ", abonne.prenom) AS NOM_Prenom
+FROM emprunt
+JOIN livre ON emprunt.id_livre = livre.id
+JOIN abonne ON emprunt.id_abonne = abonne.id
+WHERE emprunt.date_retour IS NULL AND emprunt.date_emprunt < DATE_SUB(CURDATE(), INTERVAL 2 MONTH)
+ORDER BY 2;
+
+/* exo 28 Lister tous les livres dont la dernière date d'emprunt date de plus de 2 ans */
+SELECT livre.titre
+FROM emprunt
+JOIN livre ON emprunt.id_livre = livre.id
+GROUP BY emprunt.id_livre
+HAVING MAX(emprunt.date_emprunt) < DATE_SUB(CURDATE(), INTERVAL 2 YEAR)
+ORDER BY 1;
+
+/* exo 29 Trouver les 10 abonnés ayant emprunté le plus de livres (rendus et non rendus) */
+SELECT CONCAT(UPPER(abonne.nom), " ", abonne.prenom) AS NOM_Prenom, COUNT(*)
+FROM emprunt
+JOIN abonne ON emprunt.id_abonne = abonne.id
+GROUP BY abonne.id
+ORDER BY 2 DESC
+LIMIT 10;
+
+/* exo 30 Lister les catégories les plus lues */
+SELECT livre.genre, COUNT(*)
+FROM emprunt
+JOIN livre ON emprunt.id_livre = livre.id
+GROUP BY livre.genre
+ORDER BY 2 DESC;
+
+/* exo 31 Trouver les abonnés ayant plusieurs livres en leur possession */
+SELECT abonne.nom, abonne.prenom, COUNT(*)
+FROM emprunt
+JOIN abonne ON emprunt.id_abonne = abonne.id
+WHERE emprunt.date_retour IS NULL
+GROUP BY emprunt.id_abonne
+HAVING COUNT(*) > 1
+ORDER BY 3 DESC;
+
+/* exo 32 Trouver le nombre de livre empruntés pour chaque année */
+SELECT EXTRACT(YEAR FROM date_emprunt) AS année, COUNT(*) AS nb_livre
+FROM emprunt
+GROUP BY EXTRACT(YEAR FROM date_emprunt);
+
+/* exo 33 Trouver l'âge moyen des abonnés */
+SELECT AVG(TIMESTAMPDIFF(YEAR, date_naissance, CURDATE())) AS age_moyen
+FROM abonne;
+
+/* exo 34 Trouver la ville ayant la moyenne d'âge la plus petite */
+SELECT AVG(TIMESTAMPDIFF(YEAR, date_naissance, CURDATE())) AS age_moyen, UPPER(ville)
+FROM abonne
+GROUP BY upper(ville)
+ORDER BY 1
+LIMIT 1;
+
+/* exo 35 Lister tous les abonnés qui ont des homonyme (même nom et prénom) */
+SELECT abonne.nom, abonne.prenom, COUNT(*) AS nb_homonyme
+FROM abonne
+GROUP BY nom, prenom
+HAVING COUNT(*) > 1
+ORDER BY 3 DESC, 1, 2;
+
+/* exo 36 Trouver l'âge moyen des lecteurs de chaque catégorie */
+SELECT livre.genre, AVG(TIMESTAMPDIFF(YEAR, abonne.date_naissance, CURDATE())) AS age_moyen_genre
+FROM emprunt
+JOIN livre ON emprunt.id_livre = livre.id
+JOIN abonne ON emprunt.id_abonne = abonne.id
+GROUP BY livre.genre
+ORDER BY 2;
+
+/* exo 39 Lister les 10 livres les plus empruntés (avec le nombre d’emprunt) */
